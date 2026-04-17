53e252516d310ee20bf3ec3c28a4f3a7716a3b74
voidtask <96192210+voidtask@users.noreply.github.com>
2026-04-17 20:59:57 +0400

    log errors

--- backend/src/middlewares/error-handler.ts
+++ backend/src/middlewares/error-handler.ts
@@ -1,10 +1,14 @@
 import { ErrorRequestHandler } from 'express';
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  // eslint-disable-next-line no-console
+  console.error(err);
+
   const { statusCode = 500 } = err;
   const isErrorInternal = statusCode === 500;
   const message = isErrorInternal ? 'На сервере произошла ошибка' : err.message;
   res.status(statusCode).send({ message });
+
   next();
 };
 
