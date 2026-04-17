a678e93f3dde20079a63c411e8e618ae1886b462
voidtask <96192210+voidtask@users.noreply.github.com>
2026-04-17 20:30:16 +0400

    fix useless piece of code its static anyway

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -9,13 +9,6 @@ const {
 } = process.env;
 
 module.exports = {
-  apps: [
-    {
-      name: "frontend",
-      script: "./build/index.html",
-    },
-  ],
-
   deploy: {
     production: {
       user: DEPLOY_USER,
