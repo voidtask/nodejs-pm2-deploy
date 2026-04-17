f1df295840429edfeedcaefd6c1d862579e561d9
voidtask <96192210+voidtask@users.noreply.github.com>
2026-04-17 15:13:53 +0400

    changes

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,31 @@
+require('dotenv').config({ path: '.env.deploy' });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REPO,
+  DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: 'backend',
+      script: './dist/app.js',
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy':
+        'cd backend && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+    },
+  },
+};
