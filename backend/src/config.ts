69e1558063c3668398ead19aab21fc56ffe68177
voidtask <96192210+voidtask@users.noreply.github.com>
2026-04-17 21:07:44 +0400

    please work

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,2 +1,2 @@
 export const { JWT_SECRET = 'JWT_SECRET' } = process.env;
-export const { DB_ADDRESS = 'mongodb://localhost:27017/mestodb' } = process.env;
+export const { DB_ADDRESS = 'mongodb://admin:qwerty12345@127.0.0.1:27017/mestodb?authSource=admin' } = process.env;
